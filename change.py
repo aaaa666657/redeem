1ff956371a8c7fd558100f905ce4a72603be9557
KAI <newaaaa666657@gmail.com>
2018-12-18 16:10:42 +0800

    與DB確認模式

--- change.py
+++ change.py
@@ -5,48 +5,81 @@ from flask import Flask,send_file,request
 import pymongo
 
 
+
 app = Flask(__name__, static_url_path='')
 
 
 myclient = pymongo.MongoClient('mongodb://localhost:27017/')
 dblist = myclient.list_database_names()
+DB = myclient.isweek2018.gifts
+######################################################king
 
-@app.route('/')
-def root():
-    return send_file('index.html')
-@app.route('/login', methods=['GET', 'POST']) 
-def login():
+@app.route('/know/read', methods=['GET', 'POST']) 
+def know_read():
 	if request.method == 'POST': 
-		res = myclient.test.users.find_one({"username" : request.values['username']})       	
+		res = DB.find_one({'type':'king',"username" : request.values['username']})       	
 		if res:
 			if res["redeemed"]:
 				return "已兌換過獎品"
 			else:
-				temp = myclient.test.users.find_one({"username" : request.values['username'], "redeemed" : False})
-				temp2 = temp.copy()
-				temp["redeemed"] = True
-				myclient.test.users.save(temp)
 				return "可兌換獎品"
 		else:
  			return "未參加活動"
 
 	return "<form method='post' action='/login'><input type='text' name='username' /></br><button type='submit'>Submit</button></form>"
-@app.route('/js/<path>')
-def send_js(path):
-    return send_file('js/'+path)
-@app.route('/style/<path>')
-def send_css(path):
-    return send_file('style/'+path)
-@app.route('/style/images/<path>')
-def send_css_images(path):
-    return send_file('style/images/'+path)
-@app.route('/image/<path>')
-def send_images(path):
-    return send_file('images/'+path)
-@app.errorhandler(404)
-def page_not_found(error):
-    return send_file('404.html'), 404
+
+@app.route('/know/write', methods=['GET', 'POST']) 
+def know_write():
+	if request.method == 'POST': 
+		res = DB.find_one({'type':'king',"username" : request.values['username']})       	
+		
+		if res:
+			if res["redeemed"]:
+				return "已兌換過獎品"
+			else:
+				temp2 = res.copy()
+				temp["redeemed"] = True
+				DB.save(temp)
+				return "可兌換獎品"
+		else:
+ 			return "未參加活動"
+
+	return "<form method='post' action='/login'><input type='text' name='username' /></br><button type='submit'>Submit</button></form>"
+######################################################RPG
+@app.route('/rpg/read', methods=['GET', 'POST']) 
+def rpg_read():
+	if request.method == 'POST': 
+		res = DB.find_one({'type':'rpg',"username" : request.values['username']})       	
+		if res:
+			if res["redeemed"]:
+				return "已兌換過獎品"
+			else:
+				return "可兌換獎品"
+		else:
+ 			return "未參加活動"
+
+	return "<form method='post' action='/login'><input type='text' name='username' /></br><button type='submit'>Submit</button></form>"
+
+@app.route('/rpg/write', methods=['GET', 'POST']) 
+def rpg_write():
+	if request.method == 'POST': 
+		res = DB.find_one({'type':'rpg',"username" : request.values['username']})       	
+		
+		if res:
+			if res["redeemed"]:
+				return "已兌換過獎品"
+			else:
+				temp2 = res.copy()
+				temp["redeemed"] = True
+				DB.save(temp)
+				return "可兌換獎品"
+		else:
+ 			return "未參加活動"
+
+	return "<form method='post' action='/login'><input type='text' name='username' /></br><button type='submit'>Submit</button></form>"
+
 
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0') 
+				
